820d08d837c0ff2e99ad6eceb05dd645d13f6511
eduardolucas39 <eduardolucas40@gmail.com>
2021-07-25 21:08:08 -0300

    drf-yasg installed

--- app/challenge/urls.py
+++ app/challenge/urls.py
@@ -16,10 +16,29 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Customers API",
+      default_version='v1',
+      description="Oowlish Challenge",
+      terms_of_service="https://www.myapp.com/policies/terms/",
+      contact=openapi.Contact(email="eduardolucas40@gmail.com"),
+      license=openapi.License(name="Test License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 from .views import ping
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('ping/', ping, name="ping"),
-    path("", include("customers.urls")),
+    path('customers', include('customers.urls')),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
